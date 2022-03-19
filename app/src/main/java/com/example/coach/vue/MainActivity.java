9c76600b26c6eda25cd1f58fa1cf913fe040118a
Sonia <sonia.dakhli@etu.univ-lyon1.fr>
2022-03-19 12:46:53 +0100

    Lien du bouton du menu vers une nouvelle page.

--- app/src/main/java/com/example/coach/vue/MainActivity.java
+++ app/src/main/java/com/example/coach/vue/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.coach.vue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import com.example.coach.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        creerMenu();
+    }
+
+    private void creerMenu() {
+        ecouteMenu(findViewById(R.id.btnMonIMG), CalculActivity.class);
+        //ecouteMenu(findViewById(R.id.btnMonHistorique, .class));
+    }
+
+    private void ecouteMenu(ImageButton imgButton, Class classe) {
+        imgButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, classe);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
     }
 }
